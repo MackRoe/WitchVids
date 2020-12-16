919f6479b5382b43b424ed1c6d1e7317a062afe6
MackRoe <elaine.music@students.makeschool.com>
2020-12-16 15:43:06 -0500

    update root route

--- app.py
+++ app.py
@@ -3,18 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
-@app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='WitchVids Coming Soon!!')
-
 playlists = [
     { 'title': 'Intro To Witchcraft', 'description': 'Basic Introduction & Overview' },
     { 'title': 'Pagan Comedy', 'description': 'By Pagans, For Pagans' }
 ]
 
-@app.route('/playlists')
+@app.route('/)
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists)
