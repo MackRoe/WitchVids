d9c60f2f3240619980487537f68e29fff7e89d42
MackRoe <elaine.music@students.makeschool.com>
2020-12-16 08:46:44 -0500

    boilerplate base.html & home.html

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    return 'Hello, world!'
+    return render_template('home.html', msg='WitchVids Coming Soon!!')
 
 if __name__ == '__main__':
     app.run(debug=True)
